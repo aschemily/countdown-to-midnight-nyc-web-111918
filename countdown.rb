c57bd9cbee68ef22689453a8f0f34701122d6dbb
Emily Asch <e.asch@me.com>
2019-06-19 00:40:49 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,9 @@
 #write your code here
-def count(int)
-  while int > 0 
+
+def countdown(int)
+   while int > 0 
   int -= 1
   puts "#{int} SECOND(S)!"
   end 
   puts "HAPPY NEW YEAR!"
-end 
-
-
-def countdown
 end
